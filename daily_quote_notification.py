10985a512b78c788a1e25561a0bb7b401c98ea5c
kafiq19 <39530506+kafiq19@users.noreply.github.com>
2022-05-15 12:14:54 -0300

    acsii issue fixed on quotes, removed incorporated scheduler

--- daily_quote_notification.py
+++ daily_quote_notification.py
@@ -11,31 +11,31 @@ class Daily_Quote:
         self.select_message()
         self.notification()
 
-def load_data(self):
-    csv_data = pd.read_csv('quotes.csv', names=['author', 'quote','ka', 'sch','tags'], dtype={'quote': str})
-    csv_data['sch'] = pd.to_datetime(csv_data['sch'], format='%m/%d/%Y')
-
-def select_message(self):
-    msg = csv_data.loc[random.randint(0,len(csv_data.index))]
-
-def notification(self):
-    #creates a notification email
-    today = datetime.date.today()
-    gmail_user = 'khalfeen1@gmail.com'
-    gmail_password = 'B@l@nc3G2021'
-    recipient = ['tha_realist1990@hotmail.com']
+    def load_data(self):
+        csv_data = pd.read_csv('quotes.csv', names=['author', 'quote','ka', 'sch','tags'], dtype={'quote': str})
+        csv_data['sch'] = pd.to_datetime(csv_data['sch'], format='%m/%d/%Y')
     
-    msg = EmailMessage()
-    msg['Subject'] = 'Daily Quote: ' + str(today)
-    msg['From'] = gmail_user 
-    msg['To'] = recipient 
-    msg.set_content(f'{msg.quote} \n {msg.author}')
+    def select_message(self):
+        msg = csv_data.loc[random.randint(0,len(csv_data.index))]
     
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(gmail_user, gmail_password) 
-        smtp.send_message(msg)
-
-    print("Successfully sent notification email")
+    def notification(self):
+        #creates a notification email
+        today = datetime.date.today()
+        gmail_user = 'khalfeen1@gmail.com'
+        gmail_password = 'B@l@nc3G2021'
+        recipient = ['tha_realist1990@hotmail.com']
+        
+        msg = EmailMessage()
+        msg['Subject'] = 'Daily Quote: ' + str(today)
+        msg['From'] = gmail_user 
+        msg['To'] = recipient 
+        msg.set_content(f'{msg.quote} \n {msg.author}')
+        
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(gmail_user, gmail_password) 
+            smtp.send_message(msg)
+    
+        print("Successfully sent notification email")
 
 if __name__ == "__main__":
     Daily_Quote()
