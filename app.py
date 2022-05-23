b989e899fe041bdeeffe9d8d1d037993b06e9df6
kafiq19 <39530506+kafiq19@users.noreply.github.com>
2022-05-23 11:58:17 -0300

    testing flask mail

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import random
 from datetime import date
 
 import flask
-from flask_mail import Mail #del
+from flask import Flask #del
+from flask_mail import Mail, Message #del
 from flask import request
 from flask import Flask, render_template
 from jinja2 import Template
@@ -76,7 +77,7 @@ def api_ka():
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'khalfeen1@gmail.com'
-app.config['MAIL_PASSWORD'] = 'jvofajjseaajkszt'
+app.config['MAIL_PASSWORD'] = 'B@l@nc3G2021'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
@@ -84,7 +85,7 @@ mail = Mail(app)
 
 @app.route('/email', methods=['GET'])
 def index():
-    msg = Message('Hello from the other side!', sender =   'kha@mailtrap.io', recipients = ['tha_realist1990@hotmail.com'])
+    msg = Message('Hello from the other side!', sender =   'khalfeen1@gmail.com', recipients = ['tha_realist1990@hotmail.com'])
     msg.body = "Hey Paul, sending you this email from my Flask app, lmk if it works"
     mail.send(msg)
     return "Message sent!"
